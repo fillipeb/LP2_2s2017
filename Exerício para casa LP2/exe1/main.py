48d14dbc5a9b4900e98a4961635184a1af8459ba
GabsGeorge <gabriel.g.santos@hotmail.com>
2017-11-08 20:04:18 -0200

    Ajustes

--- Exerício para casa LP2/exe1/main.py
+++ Exerício para casa LP2/exe1/main.py
@@ -6,25 +6,22 @@
 from classe_triangulos import Triangulo
 import math
 
-t1 =int(input("Digite o tamanho do 1º lado: "))
+t1=int(input("Digite o tamanho do 1º lado: "))
 t2=int(input("Digite o tamanho do 2º lado: "))
 t3=int(input("Digite o tamanho do 3º lado: "))
 triangulo1 = Triangulo(t1,t2,t3)
 
 semi_perimetro = (triangulo1.calcular_perimetro())/2
-t1 = triangulo1.ladoa
-t2 = triangulo1.ladob
-t3 = triangulo1.ladoc
+a = triangulo1.ladoa
+b = triangulo1.ladob
+c = triangulo1.ladoc
 heron = (semi_perimetro*(semi_perimetro-a)*(semi_perimetro-b)*(semi_perimetro-c))
 heron = math.sqrt(heron)
 
-# Esse print mostra a área.Tendo o perímetro em mãos, foi utilizado a fórmula de Heron 
-# para o cálculo da área.
 print(heron)
 
-maiorlado = instancia.pega_maior()
+LadoMaior = triangulo1.pega_maior()
 
-# Esse print mostra o maior lado
-print(maiorlado)
+print(LadoMaior)
 
 
--- Exerício para casa LP2/exe2/testes.py
+++ Exerício para casa LP2/exe2/testes.py
@@ -3,7 +3,7 @@
 #Lucca Marques- 1700726
 #Fillipe Borges - 1700135
 
-from funcionarios import Funcionario
+from classe_funcionarios import Funcionario
 
 harry = Funcionario('Harry',  25000)
 
